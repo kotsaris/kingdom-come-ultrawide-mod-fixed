d3a69ec885dd2c312214b885aae2b1122e236256
Kostas Konst <konstantinidis.kostas@gmail.com>
2025-11-21 21:52:09 +0200

    Working

--- bundle-copy.ps1
+++ bundle-copy.ps1
@@ -2,6 +2,12 @@ if(Test-Path -Path "MyPak.pak") {
     Remove-Item -Path "MyPak.pak"
 }
 
-7z a -tzip -mx=0 MyPak.pak data\libs\*
+Push-Location -Path ".\data"
+
+7z a -tzip -mx=0 ..\MyPak.pak libs\*
+
+Pop-Location
 
 cp .\MyPak.pak "C:\Program Files (x86)\Steam\steamapps\common\KingdomComeDeliverance\Mods\ultra_widescreen\data\data.pak"
+
+rm .\MyPak.pak
